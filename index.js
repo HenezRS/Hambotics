b806591a1eace2bc9b2a8ef27589e0619a8dd6c7
Henry Saal <henrico789@gmail.com>
2019-03-18 19:40:48 +1100

    console logging all possible routes

--- index.js
+++ index.js
@@ -19,9 +19,16 @@ class HamBox {
 window.addEventListener("load", function() {
   console.log("Hambotic: ONLINE");
 
-  let boxes = [new HamBox("A"), new HamBox("B"), new HamBox("C")];
+  let boxes = [
+    new HamBox("A"),
+    new HamBox("B"),
+    new HamBox("C"),
+    new HamBox("D")
+  ];
   generateDistCosts(boxes);
   printRoutes(boxes);
+  let routes = getAllRouteCombinations(boxes);
+  calcRoutes(boxes, routes);
 });
 
 function generateDistCosts(boxes) {
@@ -36,7 +43,11 @@ function generateDistCosts(boxes) {
           box.addDistCost(new DistCost(box.id, boxes[j].id, curDist));
         } else {
           box.addDistCost(
-            new DistCost(box.id, boxes[j].id, Math.floor(Math.random() * 100))
+            new DistCost(
+              box.id,
+              boxes[j].id,
+              1 + Math.floor(Math.random() * 20)
+            )
           );
         }
       }
@@ -46,6 +57,79 @@ function generateDistCosts(boxes) {
 
 function printRoutes(boxes) {
   console.log(boxes);
+  let main = document.getElementById("main");
+  for (let i = 0; i < boxes.length; ++i) {
+    let box = boxes[i];
+    let node = document.createElement("p");
+    let s = ""; //`Box ${box.id}:`;
+    for (let j = 0; j < box.distCost.length; ++j) {
+      s += ` [${box.distCost[j].idFrom} -> ${box.distCost[j].idTo} = ${
+        box.distCost[j].cost
+      }]`;
+    }
+
+    node.innerHTML = s;
+    main.appendChild(node);
+  }
+}
+
+function getAllRouteCombinations(boxes) {
+  let idCollection = [];
+
+  boxes.forEach(element => {
+    idCollection.push(element.id);
+  });
+
+  return combo(idCollection);
+}
+
+function calcRoutes(boxes, routes) {
+  for (let i = 0; i < routes.length; ++i) {
+    let cost = 0;
+    for (let j = 0; j < routes[i].length; ++j) {
+      if (j > 0) {
+        let box1 = getBoxById(boxes, routes[i][j - 1]);
+        cost += getDistCost(box1, routes[i][j - 1], routes[i][j]);
+        console.log(getDistCostString(box1, routes[i][j - 1], routes[i][j]));
+      }
+    }
+
+    //add the return cost to first point
+    let box1 = getBoxById(boxes, routes[i][routes[i].length - 1]);
+    cost += getDistCost(box1, routes[i][routes[i].length - 1], routes[i][0]);
+    console.log(
+      getDistCostString(box1, routes[i][routes[i].length - 1], routes[i][0])
+    );
+
+    console.log(`${routes[i]}: ${cost}`);
+  }
+}
+
+function getBoxById(boxes, id) {
+  for (let i = 0; i < boxes.length; ++i) {
+    if (boxes[i].id === id) {
+      return boxes[i];
+    }
+  }
+  return null;
+}
+
+function getDistCost(box, from, to) {
+  for (let i = 0; i < box.distCost.length; ++i) {
+    if (box.distCost[i].idFrom === from && box.distCost[i].idTo === to) {
+      return box.distCost[i].cost;
+    }
+  }
+  return null;
+}
+
+function getDistCostString(box, from, to) {
+  for (let i = 0; i < box.distCost.length; ++i) {
+    if (box.distCost[i].idFrom === from && box.distCost[i].idTo === to) {
+      return `${from}->${to}: ${box.distCost[i].cost}`;
+    }
+  }
+  return null;
 }
 
 function checkExistingDistCostBetweenBoxes(box1, box2) {
@@ -63,4 +147,37 @@ function checkExistingDistCostBetweenBoxes(box1, box2) {
   }
 
   return null;
+}
+
+//https://stackoverflow.com/questions/40654895/javascript-generating-all-combinations-of-an-array-considering-the-order
+//Author: Keith
+function combo(c) {
+  var r = [],
+    len = c.length;
+  tmp = [];
+  function nodup() {
+    var got = {};
+    for (var l = 0; l < tmp.length; l++) {
+      if (got[tmp[l]]) return false;
+      got[tmp[l]] = true;
+    }
+    return true;
+  }
+  function iter(col, done) {
+    var l, rr;
+    if (col === len) {
+      if (nodup()) {
+        rr = [];
+        for (l = 0; l < tmp.length; l++) rr.push(c[tmp[l]]);
+        r.push(rr);
+      }
+    } else {
+      for (l = 0; l < len; l++) {
+        tmp[col] = l;
+        iter(col + 1);
+      }
+    }
+  }
+  iter(0);
+  return r;
 }
